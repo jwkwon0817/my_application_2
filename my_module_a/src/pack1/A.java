ee33a5eff02e3f2e3cc204c06876a8136f3075ba
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2022-11-14 23:40:34 +0900

    Initial commit

--- my_module_a/src/pack1/A.java
+++ my_module_a/src/pack1/A.java
@@ -1,8 +1,13 @@
 package pack1;
 
+import pack2.B;
 public class A {
     // 메소드 선언
     public void method() {
         System.out.println("A() method() 선언");
+
+        // B 클래스 사용
+        B b = new B();
+        b.method();
     }
 }
